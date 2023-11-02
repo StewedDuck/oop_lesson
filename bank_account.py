098eabde890cf0a32858f403b9818d6f471468dd
Jongchana Khachatrokphai <sugarduck500@gmail.com>
2023-11-02 11:25:07 +0700

    create function delete_account

--- bank_account.py
+++ bank_account.py
@@ -21,6 +21,14 @@ class AccountDB:
                 return account
         return None
 
+    def delete_account(self, account_num):
+        index = self.__search_private(account_num)
+        if index != -1:
+            del self.account_database[index]
+        else:
+            print(num, "invalid account number; nothing to be deleted.")
+
+
     def __str__(self):
         s = ''
         for account in self.account_database:
@@ -63,5 +71,6 @@ my_account_DB.search_public("0003").deposit(50)
 print(my_account_DB)
 my_account_DB.search_public("0003").withdraw(100)
 print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
+my_account_DB.delete_account("0003")
+# my_account_DB.search_public("0010").deposit(50)
 print(my_account_DB)
